e61534fc89df06323facf9e27f9a65d593ca55aa
heraudk <72447916+heraudk@users.noreply.github.com>
2020-11-26 09:04:43 +0100

    jquery exo

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('p').animate({
+    width: '+=150px',
+    fontSize: '35px',
+    color: 'red',
+    backgroundColor: 'blue'
+}, 5000);
+
+
+$('p').animate({
+    width: '-=150px',
+    fontSize: '35px',
+    color: 'red',
+    backgroundColor: 'blue'
+}, 5000);
